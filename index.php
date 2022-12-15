7c39f8ffe67a0f26b66bf93f67f3e7dbb56f9cb7
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 17:04:54 -0500

    cambio de padding en las cajas de texto index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <?php include("includes/header.php") ?>
 
-<div class="container">
+<div class="container p-4">
     
     <div class="row">
         
@@ -10,19 +10,19 @@
 
             <div class="card card-body"> <!-- tarjeta -->
                 <form action="insertar.php" method="POST">
-                    <div class="form-group">
+                    <div class="form-group p-2">
                         <input type="text" name="nombres" class="form-control" placeholder="Nombres Estudiante" autofocus>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group p-2">
                         <input type="text" name="apellidos" class="form-control" placeholder="Apellidos Estudiante" autofocus>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group p-2">
                         <input type="text" name="edad" class="form-control" placeholder="Edad Estudiante" autofocus>
                     </div>
 
-                    <div class="form-group">
+                    <div class="form-group p-2">
                         <textarea name="descripcion" id="" rows="2" class="form-control" placeholder="Descripción"></textarea>
                     </div>
                     <input type="submit" class="btn btn-success btn-block" name="guardar_estudiante" value="Guardar">
