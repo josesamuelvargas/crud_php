07789e64b8cfb4ba5852992404936b6d6fc5efda
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 16:34:37 -0500

    creación tarjeta y campos de ingreso datos en index.php

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php include("conexion.php") ?>
 <?php include("includes/header.php") ?>
 
-    <h1>Hello World</h1>    
+<div class="container">
+    
+    <div class="row">
+        
+        <div class="col-md-4">
+
+            <div class="card card-body"> <!-- tarjeta -->
+                <form action="">
+                    <div class="form-group">
+                        <input type="text" name="nombres" class="form-control" placeholder="Nombres Estudiante" autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="apellidos" class="form-control" placeholder="Apellidos Estudiante" autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="edad" class="form-control" placeholder="Edad Estudiante" autofocus>
+                    </div>
+
+                    <div class="form-group">
+                        <textarea name="descripcion" id="" rows="2" class="form-control" placeholder="Descripción"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="guardar_estudiante" value="Guardar">
+                </form>
+            </div>
+
+        </div>
+
+        <div class="col-md-8">
+            
+        </div>
+
+    </div>
+
+</div>    
     
 <?php include("includes/footer.php") ?>
