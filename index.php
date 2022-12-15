098761dd96f414145330c93ae3f9432f1d38116a
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 15:35:35 -0500

    ordenamiento de texto en index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php include("conexion.php") ?>
-
 <?php include("includes/header.php") ?>
 
     <h1>Hello World</h1>    
-    <!-- JavaScript Bundle with Popper -->
-
+    
 <?php include("includes/footer.php") ?>
