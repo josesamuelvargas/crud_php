0808ce98f4d1f950371dd92e1871458d859edd12
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 18:36:08 -0500

    creación de consultas e inserción en BD en modulo insertar.php

--- insertar.php
+++ insertar.php
@@ -1,11 +1,19 @@
 <?php
 
-include('conexion.php');
+include("conexion.php");
 
 if (isset($_POST['insertar'])) {
-    echo 'guardando';
-}else{
-    echo 'hay error en el guardado';
+    $nombres = $_POST['nombres'];
+    $apellidos = $_POST['apellidos'];
+    $edad = $_POST['edad'];
+    $descripcion = $_POST['descripcion'];
+    
+    $query = "INSERT INTO estudiantes(nombres, apellidos, edad, descripcion) VALUES ($nombres, $apellidos, $edad, $descripcion)"; //recepción de los datos desde el formulario
+    $result = mysqli_query($conn, $query); //almacenamiento de los datos en la BD - cadena de conexión $conn y consulta $query
+    if ($result) {
+        die("Consulta fallida");
+    }
+    echo 'Guardado';
 }
 
 ?>
