0f0f30b5b173d3744dedc84e7f998cf19df6798c
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 14:18:42 -0500

    adición de carpeta includes

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php 
-    include("conexion.php");
-    $con=conectar();
-
-    $sql="SELECT *  FROM alumno";
-    $query=mysqli_query($con,$sql);
-
-    $row=mysqli_fetch_array($query);
+    echo "Hello World";
 ?>
