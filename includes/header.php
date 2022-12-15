de6fd92740416cfd8092c6a85c065dc0e0c877e3
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 16:05:59 -0500

    adición de botón de navegación en header.php

--- includes/header.php
+++ includes/header.php
@@ -9,3 +9,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">CRUD estudiantes</a>
+        </div>
+
+    </nav>
