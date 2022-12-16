2d3dc4e7dea45129a81fdc34c80e4069a990f4ff
Samuel Vargas <jvargasdaza@gmail.com>
2022-12-15 19:26:47 -0500

    adición de mensajes session en insertar.php

--- insertar.php
+++ insertar.php
@@ -8,12 +8,16 @@ if (isset($_POST['insertar'])) {
     $edad = $_POST['edad'];
     $descripcion = $_POST['descripcion'];
     
-    $query = "INSERT INTO estudiantes(nombres, apellidos, edad, descripcion) VALUES ($nombres, $apellidos, $edad, $descripcion)"; //recepción de los datos desde el formulario
+    $query = "INSERT INTO estudiantes(nombres, apellidos, edad, descripcion) VALUES ('$nombres', '$apellidos', '$edad', '$descripcion')"; //recepción de los datos desde el formulario
     $result = mysqli_query($conn, $query); //almacenamiento de los datos en la BD - cadena de conexión $conn y consulta $query
-    if ($result) {
+    if (!$result) { //si la consulta no existe o no funciona, arroja error "consulta fallida"
         die("Consulta fallida");
     }
-    echo 'Guardado';
+
+    $_SESSION['message'] = 'Datos guardados exitosamente'; // muestra mensaje (trabaja en conjunto con session de la BD en modulo conexion.php)
+    $_SESSION['message_type'] = 'success'; //esta línea es para que bootstrap asigne color automáticamente con el atributo success (success asigna color VERDE automáticamente, en cambio warning asigna color AMARILLO y danger asigna color ROJO)
+
+    header("Location: index.php");
 }
 
 ?>
