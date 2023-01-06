2df37e58f85fda79cbbf169b26e4b7fddec26a15
Jose Samuel Vargas <jvargasdaza@gmail.com>
2023-01-06 14:24:10 -0500

    adición de tabla para resultados

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
                 <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert"> <!--mensaje de alerta viene de insertar.php línea 18 (se puede reemplazar el warning por  < ? = /*$SESSION['message_type']*/ ? > -->
                     <?= $_SESSION['message']?>  <!--mensaje viene de insertar.php línea 17-->
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                </div>
+                </div> <!-- end div -->
             <?php session_unset(); }  ?> <!--fin de la alerta traida desde bootstrap /// session_unset es para limpiar la pantalla de los datos almacenados sesión-->
 
             <div class="card card-body"> <!-- tarjeta -->
@@ -28,12 +28,27 @@
                         <textarea name="descripcion" rows="2" class="form-control" placeholder="Descripción"></textarea>
                     </div>
                     <input type="submit" class="btn btn-success btn-block" name="insertar" value="Guardar">
-                </form>
-            </div>
-        </div>
-        <div class="col-md-8">            
-        </div>
-    </div>
-</main> 
+                </form> <!-- end form -->
+            </div> <!-- end div -->
+        </div> <!-- end div -->
+        <div class="col-md-8">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Nombres<th>
+                        <th>Apellidos<th>
+                        <th>Edad<th>
+                        <th>Descripción<th>
+                        <th>Fecha registro<th>
+                        <th>Acciones<th>
+                    </tr> <!-- end tr -->                   
+                </thead> <!-- end thead -->
+                <tbody> 
+                    
+                </tbody> <!-- end tbody -->
+            </table>  <!-- end table -->          
+        </div> <!-- end div -->
+    </div> <!-- end div -->
+</main> <!-- end main -->
     
 <?php include("includes/footer.php"); ?>
