d3a9271ac70a097e72cd856aabba85942f432158
Jose Samuel Vargas <jvargasdaza@gmail.com>
2023-01-06 15:29:48 -0500

    se adicionó línea de font awesome

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,8 @@
     <title>CRUD estudiantes</title>
     <!--Bootstrap 5.2.3 - CSS Only-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!--Font Awesome V.6.0-->
+    <script src="https://kit.fontawesome.com/f8b8e33076.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <nav class="navbar navbar-dark bg-dark">
