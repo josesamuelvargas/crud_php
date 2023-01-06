df1fb44d90229967e187a6dcf4341793d96cde4f
Jose Samuel Vargas <jvargasdaza@gmail.com>
2023-01-06 15:52:05 -0500

    se realiza la configuración de delete.php

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+    include("conexion.php");
+
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "DELETE FROM estudiantes WHERE id = $id";
+        $result = mysqli_query($conn, $query);
+        if($result) {
+            die("La consulta falló");
+        }
+
+        $_SESSION['message'] = 'Dato eliminado exitosamente';
+        $_SESSION['message_type'] = 'danger';
+        header("Location: index.php")
+    }
+
+
+?>
